a9542f1b0edf639d232d46d7d911f80db8d4c025
muzi55555 <muzi@lineus.kr>
2024-09-11 10:07:44 +0900

    마우스 이벤트 추가

--- src/components/MouseComponent.tsx
+++ src/components/MouseComponent.tsx
@@ -1,4 +1,26 @@
+import { useEffect, useState } from 'react';
 import styles from './MouseComponent.module.css';
+
+interface IPosition {
+  x: number;
+  y: number;
+}
+
 export default function MouseComponent() {
+  const [position, setPosition] = useState<IPosition>({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  console.log('x =>', position.x, 'y =>', position.y);
+
   return <div className={styles.boxItem}>mouse event</div>;
 }
